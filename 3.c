0bc8f1a7c339e3f1c743cd8fcd933206304229bb
DaDudeIan <iandahloliver@hotmail.com>
2020-11-18 14:23:04 +0100

    Completed 3 w/ tests

--- 3.c
+++ 3.c
@@ -16,8 +16,8 @@ int sum_tail(int n, int prev) {
     assert(n >= 1);
 
     if (n > 1) {
-        prev += n;
-        return sum_tail(n-1, prev);
+        int x = prev + n;
+        return sum_tail(n-1, x);
     } else {
         return prev;
     }
@@ -38,13 +38,29 @@ int sum_loop(int n) {
 int main(void) {
     int test_given, test_tail, test_loop, n;
 
+    printf("Test 1: n = 10. Expected result is 55:\n");
     n = 10;
 
     test_given = sum_given(n);
-
     test_tail = sum_tail(n, 1);
-
     test_loop = sum_loop(n);
+
+    assert(test_given == 55);
+    assert(test_tail == 55);
+    assert(test_loop == 55);
+
+    printf("Test given: %d\n Test tail: %d\n Test loop: %d\n", test_given, test_tail, test_loop);
+
+    printf("Test 2: n = 17. Expected result is 153:\n");
+    n = 17;
+
+    test_given = sum_given(n);
+    test_tail = sum_tail(n, 1);
+    test_loop = sum_loop(n);
+
+    assert(test_given == 153);
+    assert(test_tail == 153);
+    assert(test_loop == 153);
 
     printf("Test given: %d\n Test tail: %d\n Test loop: %d\n", test_given, test_tail, test_loop);
 
